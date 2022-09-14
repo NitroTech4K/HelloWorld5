b469a7c3faec2d95295cde2d5f699486269b10aa
NitroTech 4K <crtraer@gmail.com>
2022-09-14 14:37:08 -0300

    Added project

---
+++ HelloWorld5 Project/HelloWorld5/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow World, 5!....Again!");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to begin HDD Boot scan.");
+            Console.ReadKey(true);
+
+            Console.WriteLine("Loading...");
+
+            for (int i = 0; i < 10000; i = i + 1)
+            {
+                Console.WriteLine("Loading...");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+            }
+
+            for (int i = 0; i < 100; i = i + 1)
+            {
+                Console.WriteLine("Loading...checking BootHDD format...");
+
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.Beep(800, 100);
+                Console.WriteLine("Datasat.exe has run into a critical error and has stopped working properly. Please restart your computer, and try again");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("ERROR CODE: HDD_NTFS_HARDWARE_ERROR");
+                Console.WriteLine("");
+                Console.WriteLine("Please press Alt + F4 to stop the program");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.Beep(10000, 100);
+            }
+
+
+            
+            
+        }
+    }
+}
